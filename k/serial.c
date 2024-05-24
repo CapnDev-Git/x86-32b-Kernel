1fafc7a4088effaada17bd20fbcb9e1935499a75
Eliott Flechtner - B8 (BING) <eliott.flechtner@epita.fr>
2024-05-24 21:25:55 +0200

    refactor(serial): moved doc to header file and reorganized serial.c file

--- k/serial.c
+++ k/serial.c
@@ -20,21 +20,6 @@
 #define RDA_MASK 0x01
 #define THR_MASK 0x20
 
-/**
- * \brief Initialize the serial port for communication on COM1.
- *
- * The serial port is configured with the following settings:
- *  * No interrupts
- *  * Baud rate: 38400
- *  * Data length: 8 bits
- *  * Parity: None
- *  * Stop bits: 1
- *  * FIFO enabled
- *  * Both receiver and transmitter FIFO queues cleared
- *  * THR empty interrupt enabled
- *
- * The serial port is configured to use COM1 (0x3f8).
- */
 void serial_init(void) {
   // Disable all interrupts
   outb(SERIAL_PORT + 1, 0x00);
@@ -62,6 +47,12 @@ static int serial_received(void) {
   return inb(SERIAL_PORT + LSR_OFFSET) & RDA_MASK;
 }
 
+char serial_read(void) {
+  while (!serial_received())
+    ;
+  return inb(SERIAL_PORT);
+}
+
 /**
  * \brief Check if the serial port is ready to transmit data.
  * \return 1 if the port is ready, 0 otherwise.
@@ -70,23 +61,6 @@ static int is_transmit_empty(void) {
   return inb(SERIAL_PORT + LSR_OFFSET) & THR_MASK;
 }
 
-/**
- * \brief Read a byte from the serial port.
- * \return The byte read from the serial port.
- */
-char serial_read(void) {
-  while (!serial_received())
-    ;
-  return inb(SERIAL_PORT);
-}
-
-/**
- * \brief Write a buffer to the serial port.
- * \param buf The buffer to write.
- * \param count The number of bytes to write.
- * \return The number of bytes written.
- * \note This function blocks until all bytes are written.
- */
 int serial_write(const char *buf, size_t count) {
   for (size_t i = 0; i < count; i++) {
     // Wait until the Transmitter Holding Register is empty
@@ -98,12 +72,6 @@ int serial_write(const char *buf, size_t count) {
   }
 }
 
-/**
- * \brief Write a buffer to the serial port.
- * \param buf The buffer to write.
- * \param count The number of bytes to write.
- * \return The number of bytes written.
- */
 int write(const char *buf, size_t count) {
   serial_write(buf, count);
   return count;
--- k/serial.h
+++ k/serial.h
@@ -3,9 +3,44 @@
 
 #include <k/types.h>
 
+/**
+ * \brief Initialize the serial port for communication on COM1.
+ *
+ * The serial port is configured with the following settings:
+ *  * No interrupts
+ *  * Baud rate: 38400
+ *  * Data length: 8 bits
+ *  * Parity: None
+ *  * Stop bits: 1
+ *  * FIFO enabled
+ *  * Both receiver and transmitter FIFO queues cleared
+ *  * THR empty interrupt enabled
+ *
+ * The serial port is configured to use COM1 (0x3f8).
+ */
 void serial_init(void);
+
+/**
+ * \brief Read a byte from the serial port.
+ * \return The byte read from the serial port.
+ */
 char serial_read(void);
+
+/**
+ * \brief Write a buffer to the serial port.
+ * \param buf The buffer to write.
+ * \param count The number of bytes to write.
+ * \return The number of bytes written.
+ * \note This function blocks until all bytes are written.
+ */
 int serial_write(const char *buf, size_t count);
+
+/**
+ * \brief Write a buffer to the serial port.
+ * \param buf The buffer to write.
+ * \param count The number of bytes to write.
+ * \return The number of bytes written.
+ */
 int write(const char *buf, size_t count);
 
 #endif /* ! SERIAL_H */
