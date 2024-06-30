10a68066f3d78896435f4fd48ac1751591f49405
Eliott Flechtner - B8 (BING) <eliott.flechtner@epita.fr>
2024-06-30 14:51:16 +0200

    misc(iso9660): removed credits

--- k/include/k/iso9660.h
+++ k/include/k/iso9660.h
@@ -1,24 +1,3 @@
-/*
- *    This file is part of my_read_iso.
- *
- *    my_read_iso is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
- *
- *    my_read_iso is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with my_read_iso; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *    Copyright Alexandre Becoulet, 2003
- *    contact : alexandre.becoulet@epita.fr
- */
-
 #ifndef ISO9660_H
 #define ISO9660_H
 
